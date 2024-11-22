49f43714340666786cbe495ecbc4d55480d46d9e
Pkmnbvc <sophia.aliagarain@gmail.com>
2024-11-22 15:50:46 +0300

    added main

--- television_test.py
+++ television_test.py
@@ -7,4 +7,33 @@ class Test:
 
     def teardown_method(self):
         del self.tvl
+    def test_init(self):
+        assert self.tvl.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+    def test_init(self):
+        self.tvl.power()
+        assert self.tvl.__str__() == 'Power = True, Channel = 0, Volume = 0'
+
+        self.tvl.power()
+        assert self.tvl.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+    def test_mute(self):
+        pass
+    def test_channel_up(self):
+        self.tvl.channel_up()
+        assert self.tvl.__str__() == 'Power = False, Channel = 0, Volume = 0'
+
+        self.tvl.power()
+        self.tvl.channel_up()
+        assert self.tvl.__str__() == 'Power = True, Channel = 1, Volume = 0'
+
+        self.tvl.channel_up()
+        self.tvl.channel_up()
+        self.tvl.channel_up()
+        assert self.tvl.__str__() == 'Power = True, Channel = 0, Volume = 0'
+
+    def test_channel_down(self):
+        pass
+
+
 
